bf458474595e0867d48cff2d2771363bfee3652e
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2022-07-20 23:11:37 +0530

    Create config.py

---
+++ modules/config.py
@@ -0,0 +1,33 @@
+# 𝐌𝐨𝐝𝐮𝐥𝐞𝐬 𝐚𝐧𝐝 𝐄𝐧𝐯𝐢𝐫𝐨𝐧𝐦𝐞𝐧𝐭𝐬
+import os
+import aiohttp
+from os import getenv
+from dotenv import load_dotenv
+
+# 𝐈𝐧𝐭𝐞𝐫𝐧𝐚𝐥 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 (@Deepanshu_Meena09)
+load_dotenv()
+que = {}
+admins = {}
+aiohttpsession = aiohttp.ClientSession()
+
+# 𝐑𝐞𝐪𝐮𝐢𝐫𝐞𝐝 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 //𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 @Deepanshu_Meena09
+API_HASH = getenv("API_HASH", "XXXXX")
+API_ID = int(getenv("API_ID", "XXXXX"))
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "XXXXX")
+ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME", "XXXXX")
+BOT_IMAGE = getenv("BOT_IMAGE", "https://telegra.ph/file/d7b75110ec6c2b5efd233.jpg")
+BOT_NAME = getenv("BOT_NAME", "XXXXX")
+BOT_TOKEN = getenv("BOT_TOKEN", "12345:XXXXX")
+BOT_USERNAME = getenv("BOT_USERNAME", "XXXXX")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
+OWNER_NAME = getenv("OWNER_NAME", "Ɗᴇᴇᴘᴀɴꜱʜᴜ ᴍᴇᴇɴᴀ")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "Deepanshu_Meena09")
+SOURCE_CODE = getenv("SOURCE_CODE", "https://github.com/STD-DEEPANSHU/STD-MUSIC")
+STRING_SESSION = getenv("STRING_SESSION", "session")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5556126680").split()))
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/STD_FRIENDS_FOREVER")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "https://t.me/DEEPANSHU_WORLD")
+
+# 𝐃𝐨 𝐍𝐨𝐭 𝐂𝐡𝐚𝐧𝐠𝐞 𝐓𝐡𝐢𝐬 𝐋𝐢𝐧𝐞𝐬 // 𝐈𝐠𝐧𝐨𝐫𝐞 𝐓𝐡𝐢𝐬 (Deepanshu Meena) 
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+PROFILE_CHANNEL = getenv("PROFILE_CHANNEL", "https://t.me/girls_and_boys_dp")
